9d316d7e7bc9a1a49f80340d7efb9d7f33ba0b35
marcuspereiradev <vinnycoast@gmail.com>
2019-08-22 10:48:28 -0300

    Adding server

---
+++ src/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-fa981.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true })
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
