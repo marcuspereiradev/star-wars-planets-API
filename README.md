0597e9e0244964c647581baf5efe4ced39e8b477
marcuspereiradev <vinnycoast@gmail.com>
2019-09-02 19:24:14 -0300

    dynamic port

---
+++ README.md
@@ -0,0 +1,14 @@
+
+## Como usar
+
+### Para instalar as dependências:
+
+yarn install
+
+### No diretório deste projeto você pode iniciá-lo com:
+
+yarn dev
+
+## Authors
+
+|  [Marcus Pereira](https://github.com/marcuspereiradev)   |
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const server = express();
+const port = process.env.PORT || 3333;
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-fa981.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true })
 
@@ -13,4 +14,6 @@ server.use(cors());
 server.use(express.json());
 server.use(routes);
 
-server.listen(3333);
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
